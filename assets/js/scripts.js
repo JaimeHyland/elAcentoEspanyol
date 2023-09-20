ff56480e4fab5130e77138d30bd4cd34fe983d8b
Jaime Hyland <james.hyland@iubh.de>
2023-09-20 09:50:03 +0000

    add text and functionality to collapsibles, add code to show answer options

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,20 +1,34 @@
-async function fetchQuizFile() {
+let quizData;
+let questionCount;
 
+async function fetchQuizFile() {
     try {
         const response = await fetch('./quiz.json');
-        const data = await response.json();
-        console.log(data);
-        console.log(data.questions);
-        console.log(data.questions.length);
-        for (const key in data.questions) {
-            console.log(data.questions[key].question);
-            for (const key1 in data.questions[key].answers) {
-                for (const key2 in data.questions[key].answers[key1]) {
-                    console.log(data.questions[key].answers[key1][key2]);
-                }
+        quizData = await response.json();
+        console.log(quizData);
+        numQuestions = quizData.questions.length;
+        console.log(numQuestions);
+        for (const key in quizData.questions) {
+            console.log(quizData.questions[key].question);
+            for (const key1 in quizData.questions[key].answers) {
+                console.log(quizData.questions[key].answers[key1][0] + ': ' + quizData.questions[key].answers[key1][1]);
             }
         }
     } catch (error) {
         console.log(error);
     }
+    fillQuestion(0);
+}
+
+function createAnswerDivs(questionCount) {
+    let htmlString = "";
+    for (const i in quizData.questions[questionCount].answers) {
+        htmlString += `<div class="answer-option">` + quizData.questions[questionCount].answers[i][0] + `</div>`;
+    }
+    document.getElementById('answer-options').innerHTML = htmlString;
+}
+
+function fillQuestion(questionCount) {
+    document.getElementById('question-text').innerHTML = quizData.questions[questionCount].question;
+    createAnswerDivs(questionCount);
 }
