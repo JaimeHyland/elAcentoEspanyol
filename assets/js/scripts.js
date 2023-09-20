c80db753ef0f0625405207bdc0aa82dc6d4a5fd7
Jaime Hyland <james.hyland@iubh.de>
2023-09-20 20:28:11 +0000

    make progress on answer evaluation

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,10 @@
 let quizData;
-let questionCount;
+let numQuestions = 0;
+let currentQuestion = 0;
+let correctAnswers = 0;
+let incorrectAnswers = 0;
+
+setupClickHandler();
 
 async function fetchQuizFile() {
     try {
@@ -20,15 +25,80 @@ async function fetchQuizFile() {
     fillQuestion(0);
 }
 
-function createAnswerDivs(questionCount) {
+function createAnswerDivs(count) {
     let htmlString = "";
-    for (const i in quizData.questions[questionCount].answers) {
-        htmlString += `<div class="answer-option">` + quizData.questions[questionCount].answers[i][0] + `</div>`;
+    for (const i in quizData.questions[count].answers) {
+        console.log(quizData.questions[count].answers[i][1]);
+        // the id of the answer div identifies its position in the div array and tells whether it's the correct answer or not.
+        htmlString += `<button id="` + i + `-` + quizData.questions[count].answers[i][1] + `" class="answer-option">` + quizData.questions[count].answers[i][0] +
+            `</button>`;
+        console.log(htmlString);
     }
     document.getElementById('answer-options').innerHTML = htmlString;
 }
 
-function fillQuestion(questionCount) {
-    document.getElementById('question-text').innerHTML = quizData.questions[questionCount].question;
-    createAnswerDivs(questionCount);
+function fillQuestion(count) {
+    document.getElementById('question-text').innerHTML = quizData.questions[count].question;
+    createAnswerDivs(count);
 }
+
+// evaluate click/tap on answer-options div
+
+function setupClickHandler() {
+    document.getElementById('answer-options').addEventListener('click', function (event) {
+        // Check if clicked element is button
+        if (event.target.tagName === "BUTTON") {
+            evaluateAnswer(event.target.id);
+            // Perform your actions here
+            console.log('You clicked on the button with ID: ' + event.target.id);
+        }
+    });
+}
+
+function evaluateAnswer(idString) {
+    let answerId = idString.split("-");
+    console.log(idString);
+    console.log(answerId[1]);
+    if (answerId[1]==="true") {
+        correctAnswers++;
+        document.getElementById(idString).style.backgroundColor = 'green';
+    } else {
+        incorrectAnswers++;
+        document.getElementById(idString).style.backgroundColor = 'red';
+    }
+    console.log(correctAnswers);
+    
+    document.getElementById("running-total").innerHTML = `$correctAnswers out of $currentQuestion; still $numQuestions-currentQuestion to go!`
+}
+
+function goToNextQuestion() {
+    //    evaluateAnswer(currentQuestion);
+    //   currentQuestion ++;
+    //    correctAnswers =+ evaluateAnswer();
+    //    evaluateAnswer(currentQuestion);
+    //    totScore(currentQuestion);
+}
+
+
+
+
+function totScore(count) {
+
+}
+
+var collapsible = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < collapsible.length; i++) {
+    collapsible[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+}
+
+
